cb43cb07dbd29b46d2bdf3b87a40730c46fbcc07
Melissa Wen <wenm@usc.edu>
2019-02-13 20:41:16 +0800

    fixed plots

--- Assignment3.py
+++ Assignment3.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 
 FEATURES_FILE = "features.arff"
 
@@ -6,10 +6,14 @@ FEATURES_FILE = "features.arff"
 def main():
     # PART I: VISUALIZATION
     # Initialize the arrays to hold each parameter
-    zcr_mean_time_arr = []
-    par_mean_time_arr = []
-    zcr_std_time_arr = []
-    par_std_time_arr = []
+    zcr_mean_music_arr = []
+    par_mean_music_arr = []
+    zcr_std_music_arr = []
+    par_std_music_arr = []
+    zcr_mean_speech_arr = []
+    par_mean_speech_arr = []
+    zcr_std_speech_arr = []
+    par_std_speech_arr = []
 
     # Main loop to read in ARFF file
     with open(FEATURES_FILE, 'r') as file:
@@ -20,41 +24,51 @@ def main():
         # Extract the relevant data from each line in the file
         for line in file:
             data_arr = line.split(",")
-            zcr_mean_time = float(data_arr[0])
+
             par_mean_time = float(data_arr[1])
-            zcr_std_time = float(data_arr[7])
+            zcr_mean_time = float(data_arr[2])
             par_std_time = float(data_arr[6])
+            zcr_std_time = float(data_arr[7])
 
-            zcr_mean_time_arr.append(zcr_mean_time)
-            par_mean_time_arr.append(par_mean_time)
-            zcr_std_time_arr.append(zcr_std_time)
-            par_std_time_arr.append(par_std_time)
+            data_type = data_arr[72].strip()
+            if data_type == "music":
+                zcr_mean_music_arr.append(zcr_mean_time)
+                par_mean_music_arr.append(par_mean_time)
+                zcr_std_music_arr.append(zcr_std_time)
+                par_std_music_arr.append(par_std_time)
+            else:
+                zcr_mean_speech_arr.append(zcr_mean_time)
+                par_mean_speech_arr.append(par_mean_time)
+                zcr_std_speech_arr.append(zcr_std_time)
+                par_std_speech_arr.append(par_std_time)
 
-    print(zcr_mean_time_arr)
-    print(par_mean_time_arr)
-    print(zcr_std_time_arr)
-    print(par_std_time_arr)
-    # Plot the graphs using extracted data
-    plot_mean(zcr_mean_time_arr, par_mean_time_arr)
-    plot_std(zcr_std_time_arr, par_std_time_arr)
+    # plot the graphs using extracted data
+    plot_mean(zcr_mean_music_arr, par_mean_music_arr, zcr_mean_speech_arr, par_mean_speech_arr)
+    plot_std(zcr_std_music_arr, par_std_music_arr, zcr_std_speech_arr, par_std_speech_arr)
 
 
 # Function to plot zcr mean vs. par mean
-def plot_mean(zcr_arr, par_arr):
-    plot.plot(zcr_arr, par_arr, 'o')
-    plot.title("ZCR Mean Time vs. PAR Mean Time")
-    plot.xlabel("ZCR Mean Time")
-    plot.ylabel("PAR Mean Time")
-    plot.savefig("zcr-par-mean.png")
+def plot_mean(zcr_music, par_music, zcr_speech, par_speech):
+    plt.plot(zcr_music, par_music, 'bo', label='Music')
+    plt.plot(zcr_speech, par_speech, 'go', label="Speech")
+    plt.title("ZCR Mean Time vs. PAR Mean Time")
+    plt.xlabel("ZCR Mean Time")
+    plt.ylabel("PAR Mean Time")
+    plt.legend()
+    plt.savefig("zcr-par-mean.png")
+    plt.clf()
 
 
 # Function to plot zcr std vs. par std
-def plot_std(zcr_arr, par_arr):
-    plot.plot(zcr_arr, par_arr, 'o')
-    plot.title("ZCR Mean Time vs. PAR Mean Time")
-    plot.xlabel("ZCR Mean Time")
-    plot.ylabel("PAR Mean Time")
-    plot.savefig("zcr-par-std.png")
+def plot_std(zcr_music, par_music, zcr_speech, par_speech):
+    plt.plot(zcr_music, par_music, 'bo', label="Music")
+    plt.plot(zcr_speech, par_speech, 'go', label="Speech")
+    plt.title("ZCR Std Time vs. PAR Std Time")
+    plt.xlabel("ZCR Std Time")
+    plt.ylabel("PAR Std Time")
+    plt.legend()
+    plt.savefig("zcr-par-std.png")
+    plt.clf()
 
 
 main()
